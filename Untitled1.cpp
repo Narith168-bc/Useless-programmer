f936e110b78896a637b360930b22afd248c7db98
Narith168-bc <61857085+Narith168-bc@users.noreply.github.com>
2020-03-06 10:13:17 +0700

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
+int n;
 int main(){
     string name;
     cout<<"Enter the name:";
     cin>>name;
     cout<<"Hello "<<name<<"\t";
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(n==1){
+        cout<<"Mother fucker";
+    }
+    else
+        cout<<"Please Enter number 1";
 }
